7b6244326b5bb70950b31e46c6f4ef47602ae618
Fernando Saragoca <fsaragoca@me.com>
2016-12-14 10:37:00 -0200

    Fix sorting tags by commit date

--- spec/git_tags_action_spec.rb
+++ spec/git_tags_action_spec.rb
@@ -1,7 +1,7 @@
 describe Fastlane::Actions::GitTagsAction do
   describe 'git_tags' do
     it "executes the correct git command" do
-      allow(Fastlane::Actions).to receive(:sh).with("git tag --sort=taggerdate", anything).and_return('')
+      allow(Fastlane::Actions).to receive(:sh).with("git tag | xargs -I@ git log --format=format:\"%ai @%n\" -1 @ | sort | awk '{print $4}'", anything).and_return('')
       result = Fastlane::FastFile.new.parse("lane :test do
         git_tags
       end").runner.execute(:test)
@@ -11,7 +11,7 @@ describe Fastlane::Actions::GitTagsAction do
 
     context("with two tags") do
       before do
-        allow(Fastlane::Actions).to receive(:sh).with("git tag --sort=taggerdate", anything).and_return("v0.1.0\nv0.1.1")
+        allow(Fastlane::Actions).to receive(:sh).with("git tag | xargs -I@ git log --format=format:\"%ai @%n\" -1 @ | sort | awk '{print $4}'", anything).and_return("v0.1.0\nv0.1.1")
       end
 
       it "limits number of tags" do
